23f45153831d8797f31f7525d288bf9d6145b80a
Vladlen Soloviov <retkinf@gmail.com>
2024-05-13 17:20:32 +0300

    do hw 1. added annotation for entity. 2. implemented BookDao to BookService 3. added lombok to pom and added lombok's annotation

--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,69 +1,27 @@
 package com.example.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String author;
+    @Column(unique = true, nullable = false)
     private String isbn;
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCoverImage() {
-        return coverImage;
-    }
-
-    public void setCoverImage(String coverImage) {
-        this.coverImage = coverImage;
-    }
 }
--- src/main/java/com/example/service/BookServiceImpl.java
+++ src/main/java/com/example/service/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.service;
 
 import com.example.model.Book;
 import com.example.repository.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 
 public class BookServiceImpl implements BookService {
@@ -9,11 +10,14 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Book save(Book book) {
-        return null;
+        return bookRepository.save(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return List.of();
+        return List.of(
+                bookRepository.findAll()
+                        .orElseThrow(() -> new EntityNotFoundException("Book not found")
+                        ));
     }
 }
