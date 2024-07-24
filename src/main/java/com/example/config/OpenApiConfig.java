737be926f6e11aae05e51bd2e4bf75c2c90542b9
Vladlen Soloviov <retkinf@gmail.com>
2024-07-24 14:08:39 +0300

    fixed

--- src/main/java/com/example/config/OpenApiConfig.java
+++ src/main/java/com/example/config/OpenApiConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
     private static final String BEARER_AUTH = "BearerAuth";
+    private static final String BEARER = "bearer";
+    private static final String JWT = "JWT";
 
     @Bean
     public OpenAPI customOpenApi() {
@@ -17,8 +19,8 @@ public class OpenApiConfig {
                 .components(new Components().addSecuritySchemes(BEARER_AUTH,
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
+                                .scheme(BEARER)
+                                .bearerFormat(JWT)))
                 .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
     }
 }
